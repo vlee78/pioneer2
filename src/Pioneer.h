aa73e70bf359d4e8f068c32054753a22718dfe97
shengcunli <shengcunli@tencent.com>
2020-06-21 02:03:16 +0800

    decode video

--- src/Pioneer.h
+++ src/Pioneer.h
@@ -9,6 +9,7 @@ namespace pioneer
 		static int testMediaInfo(int argc, const char* argv[]);
 		static int testDecodeMp2(int argc, const char* argv[]);
 		static int testDecodeAudio(int argc, const char* argv[]);
+		static int testDecodeVideo(int argc, const char* argv[]);
 
 		static int testPioneer(int argc, const char* argv[]);
 	};
