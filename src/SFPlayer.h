da5fbaad8dd5ea264e34590f58f7e49f29f871c7
vlee78 <vlee78@189.cn>
2020-11-11 02:21:19 +0800

    bak

--- src/SFPlayer.h
+++ src/SFPlayer.h
@@ -5,10 +5,25 @@ namespace pioneer
 	class SFPlayer
 	{
 	public:
+		enum State
+		{
+			Ready		= 0,
+			Paused		= 1,
+			Buffering	= 2,
+			Playing		= 3,
+		};
+
+	public:
 		SFPlayer();
 		~SFPlayer();
 		long long Init(const char* filename, bool videoEnabled = true, bool audioEnabled = true);
 		void Uninit();
+
+		State GetState();
+		long long PausePlay(bool play);
+		float GetCurrent();
+		float GetTotal();
+		long long SeekTo(float seek);
 
 	private:
 		class SFPlayerImpl;
